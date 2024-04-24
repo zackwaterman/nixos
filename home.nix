f223007fadfaee9704216747f539c27371e0b2b2
Zack Waterman <zack.waterman@watermancs.com>
2024-04-24 15:16:32 -0700

    comment

--- home.nix
+++ home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "zack";
   home.homeDirectory = "/home/zack";
 
